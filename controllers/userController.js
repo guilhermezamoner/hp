282f64428182919ff4eb5207eddae7b4a0087d50
Guilherme Z <zamonerguilherme@gmail.com>
2018-07-08 21:11:56 -0300

    Refatorado métodos para o Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,57 @@
 var mongoose = require("mongoose");
 var User = require("../models/user");
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
 
 var userController = {};
+
+userController.register = function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('name', 'Nome é obrigatório!').notEmpty();
+  req.checkBody('email', 'Email é obrigatório!').notEmpty();
+  req.checkBody('email', 'Email não válido!').isEmail();
+  req.checkBody('username', 'Username é obrigatório!').notEmpty();
+  req.checkBody('password', 'Senha é obrigatório!').notEmpty();
+  req.checkBody('password2', 'Senhas não conferem!').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('register', {
+      errors:errors
+    });
+  } else {
+    let newUser = new User({
+      name:name,
+      email:email,
+      username:username,
+      password:password
+    });
+
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success','You are now registered and can log in');
+            res.redirect('/users/login');
+          }
+        });
+      });
+    });
+  }
+};
 
 userController.edit = function(req, res) {
   User.findOne({_id: req.params.id}).exec(function (err, user) {
@@ -26,6 +75,18 @@ userController.update = function(req, res) {
       res.render("../views/user/edit", {user: req.body});
     }
     res.redirect("/");
+  });
+};
+
+userController.delete = function(req, res) {
+  User.remove({_id: req.params.id}, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log("Usuário Deletado!");
+      res.redirect("/");
+    }
   });
 };
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+var userController = require("../controllers/userController.js");
 const passport = require('passport');
 
 // Bring in User Model
@@ -13,51 +13,7 @@ router.get('/register', function(req, res){
 
 // Register Proccess
 router.post('/register', function(req, res){
-  const name = req.body.name;
-  const email = req.body.email;
-  const username = req.body.username;
-  const password = req.body.password;
-  const password2 = req.body.password2;
-
-  req.checkBody('name', 'Name is required').notEmpty();
-  req.checkBody('email', 'Email is required').notEmpty();
-  req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('username', 'Username is required').notEmpty();
-  req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
-
-  let errors = req.validationErrors();
-
-  if(errors){
-    res.render('register', {
-      errors:errors
-    });
-  } else {
-    let newUser = new User({
-      name:name,
-      email:email,
-      username:username,
-      password:password
-    });
-
-    bcrypt.genSalt(10, function(err, salt){
-      bcrypt.hash(newUser.password, salt, function(err, hash){
-        if(err){
-          console.log(err);
-        }
-        newUser.password = hash;
-        newUser.save(function(err){
-          if(err){
-            console.log(err);
-            return;
-          } else {
-            req.flash('success','You are now registered and can log in');
-            res.redirect('/users/login');
-          }
-        });
-      });
-    });
-  }
+  userController.register(req, res);
 });
 
 // Login Form
@@ -82,11 +38,15 @@ router.get('/logout', function(req, res){
 });
 
 router.get('/edit/:id', function(req, res) {
-  user.edit(req, res);
+  userController.edit(req, res);
 });
 
 router.post('/update/:id', function(req, res) {
-  user.update(req, res);
+  userController.update(req, res);
+});
+
+router.get('/delete/:id', function(req, res, next) {
+  userController.delete(req, res);
 });
 
 module.exports = router;
